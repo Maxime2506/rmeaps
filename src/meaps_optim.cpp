f983d914b9fc7b4d2159bb0c809d7c61ffd49567
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2024-04-19 15:41:06 +0200

    Update meaps_optim.cpp

--- src/meaps_optim.cpp
+++ src/meaps_optim.cpp
@@ -14,37 +14,37 @@ using namespace Rcpp;
 
 // Remarque pour les fonctions pénalités: multiplier la fonction par un facteur arbitraire ne change pas le résultat de meaps.
 //' Fonction de pénalité "marche" : vaut 1 sur un rayon fixé, et decru au-delà.
-//' @param x distance.
-//' @param rayon distance de la marche.
-//' @param plancher point bas après la marche.
-//' 
-//' @return un facteur d'attraction
+ //' @param x distance.
+ //' @param rayon distance de la marche.
+ //' @param plancher point bas après la marche.
+ //' 
+ //' @return un facteur d'attraction
  inline double marche(double x, const double rayon, const double plafond) {
    if (x > rayon) return 1;
    return plafond;
  }
  
-//' Fonction de pénalité "marche_liss" : vaut 1 sur un rayon fixé, et décroit au-delà,
-//' en lissant le passge pour les distances fractionaires
-//' @param x distance.
-//' @param rayon distance de la marche.
-//' @param plancher point bas après la marche.
-//' 
-//' @return un facteur d'attraction
+ //' Fonction de pénalité "marche_liss" : vaut 1 sur un rayon fixé, et décroit au-delà,
+ //' en lissant le passge pour les distances fractionaires
+ //' @param x distance.
+ //' @param rayon distance de la marche.
+ //' @param plancher point bas après la marche.
+ //' 
+ //' @return un facteur d'attraction
  inline double marche_liss(double x, const double rayon, const double plafond) {
    if (x > ceil(rayon)) return 1;
    if (x <= floor(rayon)) return plafond;
    return plafond + (1 - plafond) *  (ceil(rayon) - rayon);
  }
  
-//' Fonction de pénalité "double_marche_liss" : vaut 1 au delà de r1+r2+1, et prend deux valeurs (o1, o2),
-//' lorsque x<=r1 et r1<x<=r2+r1,
-//' en lissant le passage pour les distances fractionaires
-//' @param x distance.
-//' @param rayon distance de la marche.
-//' @param plancher point bas après la marche.
-//' 
-//' @return un facteur d'attraction
+ //' Fonction de pénalité "double_marche_liss" : vaut 1 au delà de r1+r2+1, et prend deux valeurs (o1, o2),
+ //' lorsque x<=r1 et r1<x<=r2+r1,
+ //' en lissant le passage pour les distances fractionaires
+ //' @param x distance.
+ //' @param rayon distance de la marche.
+ //' @param plancher point bas après la marche.
+ //' 
+ //' @return un facteur d'attraction
  inline double marche_liss(double x, const double r1, const double r2,
                            const double o1, const double o2) {
    if (x > ceil(r1+r2)) return 1;
@@ -55,12 +55,12 @@ using namespace Rcpp;
  }
  
  
-//' Fonction de pénalité "decay" : odd = 1/d^delta + plancher,
-//' @param x distance.
-//' @param delta, exposant de la distance.
-//' @param plancher valeur pour les distances infinies.
-//' 
-//' @return un facteur d'attraction
+ //' Fonction de pénalité "decay" : odd = 1/d^delta + plancher,
+ //' @param x distance.
+ //' @param delta, exposant de la distance.
+ //' @param plancher valeur pour les distances infinies.
+ //' 
+ //' @return un facteur d'attraction
  inline double decay(double x, const double delta, const double plancher) {
    return exp(-delta * log(x)) + plancher;
  }
@@ -229,6 +229,7 @@ using namespace Rcpp;
          if (attraction == "double_marche_liss") {
            for (std::size_t k = 0; k < k_valid; ++k) {
              facteur_attraction[k]  = marche_liss(_xr_dist[debut + k], parametres[0], parametres[1], parametres[2], parametres[3]);
+           }}
          
          if (attraction == "decay") {
            for (std::size_t k = 0; k < k_valid; ++k) {
