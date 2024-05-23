fdfbc058ba00f287f4b00c84c288b3e645bacbf3
Maxime PARODI <maxime.parodi@sciencespo.fr>
2024-05-23 15:30:09 +0200

    correction mineure

--- R/emiette.R
+++ R/emiette.R
@@ -5,13 +5,13 @@
 #' @param nshuf le nombre de tirages
 #' @param seuil le seuil d'emmiettage
 #' @param var si les actifs sont un tibble, le nom de la variable 'actifs' ('actifs' par défaut)
-#' @param weighted si TRUE, les actifs sont pondérés par leur fréquence (TRUE par défaut)
+#' @param seed la table de hasard pour le tirage des shufs
 #'
 #' @return une matrice d'arrangement (autant de lignes que nshuf, autant de colonnes que d'actifs, dont les colonnes sont l'idINS)
 #' @export
 #'
 
-emiette <- function(les_actifs, nshuf = 256, seuil=40, var = "actifs", weighted=TRUE) {
+emiette <- function(les_actifs, nshuf = 256, seuil=40, seed = NULL, var = "actifs") {
   if(tibble::is_tibble(les_actifs))
     act <- les_actifs |> 
       dplyr::pull(var, name = idINS)
@@ -19,14 +19,21 @@ emiette <- function(les_actifs, nshuf = 256, seuil=40, var = "actifs", weighted=
     act <- les_actifs
   
   freq <- act %/% seuil + 1
-  les_labels <- rep(act, freq)
+  les_miettes <- purrr::map2_dbl(rep(act, freq), rep(freq, freq), \(n, f) n/f)
   
-  shuf <- matrix(NA, ncol = sum(freq), nrow = nshuf)
-  if(weighted==FALSE)
-    w <- NULL
+  N <- length(les_miettes)
+  
+  if(is.null(seed)) seed <- round(runif(1, 1, 1000))
+  set.seed(seed)
+  
+  shuf <- matrix(NA, ncol = N, nrow = nshuf)
   for(i in 1:nshuf) 
-    shuf[i, ] <- sample(set, sum(freq), prob = w, replace=FALSE)
-  colnames(shuf) <- les_labels
+    shuf[i, ] <- sample(0:(N-1L), sum(freq), prob = les_miettes, replace = FALSE) |> as.integer()
+  colnames(shuf) <- names(les_miettes)
+  
+  attr(shuf, "seed") <- seed
+  attr(shuf, "seuil") <- seuil
+  
   return(shuf)
 }
 
